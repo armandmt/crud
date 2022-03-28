639ab2db64aa9961ea598eb968fefc485999c34f
armandmt <armandmt@gmail.com>
2022-03-29 00:10:37 +0200

    qqqq

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
-    <App />,document.getElementById('root')
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={ <Menu/>}>
+            <Route index element= {<App/>} />
+            <Route path="about" element= {<About/>} />
+            <Route path="login" element= {<Login/>} />
+            <Route path="*" element= {<App/>} />
+        </Route>
+    </Routes>
+    <App />
+    </BrowserRouter>
+    ,document.getElementById('root')
 );
 
